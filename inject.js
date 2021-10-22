02e0e1ca4a77ebd93a9860f3ea715b3a2a68c749
EnzoRobaina <enzo.robaina@gmail.com>
2021-10-22 18:57:42 -0300

    Wraps inject.js in a self calling function to prevent global constants

--- inject.js
+++ inject.js
@@ -1,29 +1,31 @@
-const domain = psl.get(window.location.hostname)
-const hostname = window.location.hostname
-
-if (domain) {
-    chrome.storage.local.get({ '__injector': [] }, function(result) {
-        const found = result['__injector'].filter(r => {
-            if (r?.match_sub) {
-                return domain === r.url
+(function () {
+    const domain = psl.get(window.location.hostname)
+    const hostname = window.location.hostname
+    
+    if (domain) {
+        chrome.storage.local.get({ '__injector': [] }, function(result) {
+            const found = result['__injector'].filter(r => {
+                if (r?.match_sub) {
+                    return domain === r.url
+                }
+    
+                return hostname === r.url
+            })?.[0]
+    
+            if (!found) { return }
+    
+            if (found?.enabled && found?.code) {
+                try {
+                    eval(found.code)
+                    chrome.extension.sendMessage(
+                        { kind: 'badge' }, 
+                        () => {}
+                    )
+                }
+                catch (e) {
+                    console.error('Exception thrown when evaluating your js. Check the syntax.')
+                }
             }
-
-            return hostname === r.url
-        })?.[0]
-
-        if (!found) { return }
-
-        if (found?.enabled && found?.code) {
-            try {
-                eval(found.code)
-                chrome.extension.sendMessage(
-                    { kind: 'badge' }, 
-                    () => {}
-                )
-            }
-            catch (e) {
-                console.error('Exception thrown when evaluating your js. Check the syntax.')
-            }
-        }
-    })
-}
+        })
+    }
+}())
