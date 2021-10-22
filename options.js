bf435934f37e9f75f34a65aae021e065067214e2
EnzoRobaina <enzo.robaina@gmail.com>
2021-10-22 14:25:53 -0300

    Improves options page

--- options.js
+++ options.js
@@ -1,5 +1,10 @@
-const q = (query) => {
-    return document.querySelectorAll(query)
+const q = (query, expectingSingleResult = false) => {
+    const r = document.querySelectorAll(query)
+    if (!expectingSingleResult) {
+        return r;
+    }
+
+    return (r?.length === 1) ? r[0] : r 
 }
 
 const c = (tag, options = {}) => {
@@ -23,33 +28,102 @@ const fn = () => {}
 
 const addToStorage = (obj, callback = fn) => {
     chrome.storage.local.get({ "__injector": {} }, function(result) {
-        const mergedResult = {...result["__injector"], ...obj}
+        const mergedResult = { ...result["__injector"], ...obj }
         chrome.storage.local.set({ "__injector": mergedResult }, callback)
     })
 }
 
-document.addEventListener('DOMContentLoaded', function(e) {
-    document.body.style.background = 'red'
+const removeFromStorage = (domain, callback = fn) => {
+    chrome.storage.local.get({ "__injector": {} }, function(result) {
+        const newResult = { ...result["__injector"] }
+        delete newResult[domain]
 
-    const save = q('#save')[0]
+        chrome.storage.local.set({ "__injector": newResult }, callback)
+    })
+}
+
+const appendNewItem = (data = null) => {
+    const template = q('#item', true)
+    const scroller = q('#scroller', true)
+
+    const newItem = template.content.cloneNode(true)
+
+    newItem.querySelector('button.remove-me').addEventListener('click', function(e) {
+        const parent = e.target.parentElement
+        const domain = parent.querySelector('[name="domain"]').value
+        if (domain) {
+            removeFromStorage(domain, () => {
+                parent.remove()
+            })
+        }
+        else {
+            parent.remove()
+        }
+    })
+
+    if (data) {
+        newItem.querySelector('[name="domain"]').value = data?.domain
+        newItem.querySelector('[name="code"]').value = data?.script
+        if (data?.match_sub) {
+            newItem.querySelector('[name="match_sub"]').setAttribute('checked', true)
+        }
+        if (data?.enabled) {
+            newItem.querySelector('[name="enabled"]').setAttribute('checked', true)
+        }
+    }
+    scroller.appendChild(newItem)
+    scroller.scrollTop = scroller.scrollHeight
+}
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    document.body.style.background = 'gray'
+
+    const add = q('#add', true)
+    const save = q('#save', true)
+
+    save.setAttribute('disabled', true)
+    add.setAttribute('disabled', true)
+
+    chrome.storage.local.get({ "__injector": {} }, function(result) {
+        if (!Object.keys(result["__injector"])?.length) {
+            appendNewItem()
+        }
+        else {
+            Object.entries(result["__injector"]).forEach(entry => {
+                appendNewItem({ domain: entry[0], ...entry[1] })
+            })
+        }
+
+        save.removeAttribute('disabled')
+        add.removeAttribute('disabled')
+    })
+
+    add.addEventListener('click', function(e) {
+        appendNewItem()
+    })
+
     save.addEventListener('click', (e) => {
         console.log('clicked')
         const itens = q('div.row.item')
         itens.forEach(item => {
-            const domain = item.querySelector('input[type="text"]').value
-            const script = item.querySelector('textarea').value
-            const enabled = item.querySelector('input[type="checkbox"]').checked
+            const domain = item.querySelector('[name="domain"]').value
 
-            const obj = {}
-
-            obj[domain] = { script, enabled }
-
-            addToStorage(obj, () => { 
-                save.style.background = 'black'
-                setTimeout(() => {
-                    save.style.background = 'green'
-                }, 1000)
-            })
+            if (domain) {
+                const script = item.querySelector('[name="code"]').value
+                const match_sub = item.querySelector('[name="match_sub"]').checked
+                const enabled = item.querySelector('[name="enabled"]').checked
+    
+                const obj = {}
+    
+                obj[domain] = { script, match_sub, enabled }
+    
+                addToStorage(obj, () => { 
+                    save.style.background = 'black'
+                    setTimeout(() => {
+                        save.style.background = 'green'
+                    }, 1000)
+                })
+            }
         })        
     })
 })
